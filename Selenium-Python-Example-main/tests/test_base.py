3c4c633e72a149764b1b91eac4d27be90165bb34
mahmood4 <mahmood4@gmail.com>
2022-02-26 16:07:08 -0500

    Project1

---
+++ Selenium-Python-Example-main/tests/test_base.py
@@ -0,0 +1,18 @@
+import pytest
+
+# https://stackoverflow.com/questions/63292928/add-pytest-fixtures-to-a-test-class-using-dependency-injection
+# /63436993#63436993
+from utility.excel_parser import ExcelParser
+from utility.json_parser import JsonParser
+
+
+#Selenium-Python-Example-main/utility/config_parser.py
+
+class BaseTest:
+    @pytest.fixture(autouse=True)
+    def injector(self, pages, prep_properties):
+        # instantiates pages object, and data readers
+        self.pages = pages
+        self.json_reader = JsonParser("tests_data.json")
+        self.config_reader = prep_properties
+        self.excel_reader = ExcelParser("data.xlsx")
